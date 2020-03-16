199a0b7ca9e9dd4ffb949af6c38534810a631bd1
Atsuki-Shijo <a.shijo@ls-style.co.jp>
2020-03-17 01:02:23 +0900

    【四條】現在、修正中です。

--- src/main/java/jp/co/lsstyle/y/hiruta/MemoInpController.java
+++ src/main/java/jp/co/lsstyle/y/hiruta/MemoInpController.java
@@ -1,25 +1,44 @@
 package jp.co.lsstyle.y.hiruta;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemoInpController {
-
-	@RequestMapping(value="/MemoInp", method = RequestMethod.GET)
-	private String MemoInp() {
+	@RequestMapping(value="/memoInp", method = RequestMethod.GET)
+	private String memoInp() {
 		return "/MemoInp.html";
 	}
 
-	@RequestMapping(value = "memoform", method = RequestMethod.GET)
-	public String output1(
-		@RequestParam(value = "TextMemo") String memoStr,
-		Model model) {
+	@RequestMapping(value="/memoform", method = RequestMethod.POST)
+	public ModelAndView memo(@ModelAttribute("MemoInpForm") MemoInpForm form) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("TextMemo", memo(form));
+		modelAndView.setViewName("/MemoOut.html");
+		return modelAndView;
 
-		model.addAttribute("memo1", memoStr);
-		return "/MemoOut.html";
-	}
+		}
+
+//
+//	@RequestMapping(value = "memoform", method = RequestMethod.POST)
+//	public String output1(
+//		@RequestParam(value = "TextMemo") String memoStr,
+//		Model model) {
+//
+//		model.addAttribute("memo1", memoStr);
+//		return "/MemoOut.html";
+//	}
+
+//	@RequestMapping(value = "memoform", method = RequestMethod.GET)
+//	public ModelAndView memo(Object memoStr) {
+//		ModelAndView modelAndView = new ModelAndView("MemoInp");
+//		modelAndView.addObject("memo1",memoStr);
+//		return modelAndView;
+//	}
+
 }
---
+++ src/main/java/jp/co/lsstyle/y/hiruta/MemoInpForm.java
@@ -0,0 +1,12 @@
+package jp.co.lsstyle.y.hiruta;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemoInpForm {
+
+	//テキスト
+	private String TextMemo;
+}
