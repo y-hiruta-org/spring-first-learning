9295a153141cdbb5bbd5a32754b5eff506d1d4b8
Atsuki-Shijo <a.shijo@ls-style.co.jp>
2020-04-03 00:11:23 +0900

    【四條】4/2 comit アウトプット用controllerの保存ボタン対応

--- src/main/java/jp/co/lsstyle/y/hiruta/MemoOutController.java
+++ src/main/java/jp/co/lsstyle/y/hiruta/MemoOutController.java
@@ -8,17 +8,25 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MemoOutController {
 
-//	@RequestMapping(value="/memoOut", method = RequestMethod.GET)
-//	private ModelAndView memoOut(ModelAndView model, MemoOutForm form) {
-//		model.addObject("memoOutForm", form);
-//		model.setViewName("/MemoOut.html");
-//		return model;
-//	}
-
-	@RequestMapping(value="memoOutButton",params = "memoBack", method = RequestMethod.POST)
+	//保存ボタン
+	@RequestMapping(value="memoOutButton",params = "memoSave", method = RequestMethod.POST)
 	private ModelAndView memoSave(ModelAndView model, MemoOutForm form) {
 		model.addObject("memoOutForm", form);
+		model.setViewName("/MemoOut.html");
+		return model;
+	}
+
+	//戻るボタン
+	@RequestMapping(value="memoOutButton",params = "memoBack", method = RequestMethod.POST)
+	private ModelAndView memoBack(ModelAndView model) {
 		model.setViewName("/MemoInp.html");
+		return model;
+	}
+
+	//トップメニューへ
+	@RequestMapping(value="memoOutButton",params = "topBack", method = RequestMethod.POST)
+	private ModelAndView topBack(ModelAndView model) {
+		model.setViewName("/TopMenu.html");
 		return model;
 	}
 
