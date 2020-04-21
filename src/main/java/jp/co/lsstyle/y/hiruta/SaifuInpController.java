c14f49540e34d7911ebc9c6dc4e0978802fe96bf
Atsuki-Shijo <a.shijo@ls-style.co.jp>
2020-04-22 01:17:37 +0900

    【四條】4/22 comit ModelAndView編、金種枚数プログラムの実装【計算方法の変更中】

--- src/main/java/jp/co/lsstyle/y/hiruta/SaifuInpController.java
+++ src/main/java/jp/co/lsstyle/y/hiruta/SaifuInpController.java
@@ -1,7 +1,5 @@
 package jp.co.lsstyle.y.hiruta;
 
-//import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +16,7 @@ public class SaifuInpController {
 		return model;
 	}
 
-	//RequestMappingでTestInp.htmlのtestInpFormを受け取る
+	//RequestMappingで受け取る
 	@RequestMapping(value = { "/saifuInpForm" }, method = RequestMethod.GET)
 	public ModelAndView output1(
 
@@ -26,19 +24,64 @@ public class SaifuInpController {
 		//formを受け取る
 		@ModelAttribute("saifuForm") ModelAndView model,
 		//金額
-		@RequestParam(name = "atai1") int ataiOne) {
+		@RequestParam(name = "atai1") String ataiOne) {
 
-//			ArrayList<Integer> okaneList = new ArrayList<>();
+			String kingakuStr = ataiOne;
+			int kazuInt;
 
-			int [] kinshuInt = {10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1};
-
-			int kingakuInt = ataiOne;
-
-			for (int i = 0 ; i < kinshuInt.length ; i++) {
-				int maisuInt = kingakuInt / kinshuInt[i];
-				model.addObject("Atai1", kinshuInt[i]);
-				model.addObject("Atai2", maisuInt);
-				kingakuInt = kingakuInt % kinshuInt[i];
+			kazuInt = Integer.parseInt(kingakuStr) / 10000;
+			if (kazuInt == 0) {
+				model.addObject("Atai1",0);
+			}else {
+				model.addObject("Atai1", kazuInt);
+			}
+			kazuInt = Integer.parseInt(kingakuStr.substring(1)) / 5000;
+			if (kazuInt == 0) {
+				model.addObject("Atai2",0);
+			}else {
+				model.addObject("Atai2", kazuInt);
+			}
+			kazuInt = Integer.parseInt(kingakuStr.substring(1)) / 1000;
+			if (kazuInt == 0) {
+				model.addObject("Atai3",0);
+			}else {
+				model.addObject("Atai3", kazuInt);
+			}
+			kazuInt = Integer.parseInt(kingakuStr.substring(2)) / 500;
+			if (kazuInt == 0) {
+				model.addObject("Atai4",0);
+			}else {
+				model.addObject("Atai4", kazuInt);
+			}
+			kazuInt = Integer.parseInt(kingakuStr.substring(2)) / 100;
+			if (kazuInt == 0) {
+				model.addObject("Atai5",0);
+			}else {
+				model.addObject("Atai5", kazuInt);
+			}
+			kazuInt = Integer.parseInt(kingakuStr.substring(3)) / 50;
+			if (kazuInt == 0) {
+				model.addObject("Atai6",0);
+			}else {
+				model.addObject("Atai6", kazuInt);
+			}
+			kazuInt = Integer.parseInt(kingakuStr.substring(3)) / 10;
+			if (kazuInt == 0) {
+				model.addObject("Atai7",0);
+			}else {
+				model.addObject("Atai7", kazuInt);
+			}
+			kazuInt = Integer.parseInt(kingakuStr.substring(4)) / 5;
+			if (kazuInt == 0) {
+				model.addObject("Atai8",0);
+			}else {
+				model.addObject("Atai8", kazuInt);
+			}
+			kazuInt = Integer.parseInt(kingakuStr.substring(4)) / 1;
+			if (kazuInt == 0) {
+				model.addObject("Atai9",0);
+			}else {
+				model.addObject("Atai9", kazuInt);
 			}
 
 			model.setViewName("/saifuOut");
